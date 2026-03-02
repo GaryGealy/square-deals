51a4d920435d34bac46ea0921216a062ad8553a2
Gary Gealy <GaryGealy@users.noreply.github.com>
2026-03-01 18:24:15 -0600

    chore: add ESLint and Prettier, format codebase

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -9,6 +9,6 @@ export type WithElementRef<T, E extends HTMLElement = HTMLElement> = T & {
 	ref?: E | null;
 };
 
-export type WithoutChild<T> = Omit<T, "child">;
-export type WithoutChildren<T> = Omit<T, "children">;
-export type WithoutChildrenOrChild<T> = Omit<T, "children" | "child">;
+export type WithoutChild<T> = Omit<T, 'child'>;
+export type WithoutChildren<T> = Omit<T, 'children'>;
+export type WithoutChildrenOrChild<T> = Omit<T, 'children' | 'child'>;
